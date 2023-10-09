f345af44a14b7dff57fc1cbbe12900cabcee3978
QQWaseokE <waseoke@gmail.com>
2023-10-09 18:19:02 +0900

    레이아웃 구성

--- app/src/main/java/com/waseokelab/composetutorial/MainActivity.kt
+++ app/src/main/java/com/waseokelab/composetutorial/MainActivity.kt
@@ -7,10 +7,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -26,14 +36,25 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: com.waseokelab.composetutorial.Message) {
-    Row{
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)){
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture"
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(RectangleShape)
         )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
 
         Column{
             Text(text = msg.author)
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
