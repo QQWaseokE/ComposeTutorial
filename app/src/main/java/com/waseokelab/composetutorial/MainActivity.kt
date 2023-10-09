fc35222af9177f68e947cb484c7c58c4fc21cb59
QQWaseokE <waseoke@gmail.com>
2023-10-09 17:30:34 +0900

    Initial commit

---
+++ app/src/main/java/com/waseokelab/composetutorial/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.waseokelab.composetutorial
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.waseokelab.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Android")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard("Android")
+}
