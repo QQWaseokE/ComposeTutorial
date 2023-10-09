180aed426b6f5cbf94be7f78a295fe5304cf5050
QQWaseokE <waseoke@gmail.com>
2023-10-09 18:06:52 +0900

    이미지 요소 추가

--- app/src/main/java/com/waseokelab/composetutorial/MainActivity.kt
+++ app/src/main/java/com/waseokelab/composetutorial/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.waseokelab.composetutorial
 
 import android.os.Bundle
+import androidx.compose.foundation.Image
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 
@@ -23,9 +26,16 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: com.waseokelab.composetutorial.Message) {
-    Column{
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row{
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture"
+        )
+
+        Column{
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
     }
 }
 
@@ -33,6 +43,6 @@ fun MessageCard(msg: com.waseokelab.composetutorial.Message) {
 @Composable
 fun PreviewMessageCard(){
     MessageCard(
-        msg = Message("Colleague", "Hey, take a look at Jetpack Compose")
+        msg = Message("Android", "Jetpact Comopose")
     )
 }
